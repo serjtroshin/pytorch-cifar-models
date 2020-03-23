df8a0397a69e44376e9f467efe988482f56a7815
serjtroshin <serj.troshin2013@yandex.ru>
2020-03-23 21:40:07 +0300

    refactor logs

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
+set -e
+
 cat $0
-CUDA_VISIBLE_DEVICES=1,2,3 python main.py --epoch 160 --batch-size 128 --lr 0.1 --momentum 0.9 -ct 10
+
+python main.py \
+--epoch 160 \
+--batch-size 128 \
+--lr 0.1 \
+--momentum 0.9 \
+-ct 10 \
+--name baseline
